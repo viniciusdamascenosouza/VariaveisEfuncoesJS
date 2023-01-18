37519ef578d2e07e83e77c4d8557d9fad93d82ba
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-18 18:31:28 -0300

    type-string

---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá, mundo!"
+const texto2 = 'Olá, mundo!'
+
+const senhaSuperSegura = "senhasegura4321--"
+
+const stringDeNumeros = "859384849"
+
+const citacao = 'O Vinícius disse "Oi"'
+console.log(citacao)
+
+
+
+// template string ou template literal
+
