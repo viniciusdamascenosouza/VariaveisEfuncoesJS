1c2a47631ba14fc5ca6faf8b1f6017fec236a112
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-18 18:39:29 -0300

    concatenação

--- type-string.js
+++ type-string.js
@@ -8,7 +8,14 @@ const stringDeNumeros = "859384849"
 const citacao = 'O Vinícius disse "Oi"'
 console.log(citacao)
 
+//concatenação(+)
+const fraseApersentacao = "Meu nome é "
+const nome = "Vinícius"
+const sobrenome = " Damasceno"
 
+console.log(fraseApersentacao + nome + sobrenome)
 
 // template string ou template literal
+// forma mais fácio para formar frases e buscar variaveis para jogar dentro de uma item
+`Meu nome é ${nome} e meu sobrenome é ${sobrenome}`
 
