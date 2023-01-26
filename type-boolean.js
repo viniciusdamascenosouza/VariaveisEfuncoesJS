f3b1c0fed33205f9163b7151ad99cc0b756b2a18
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-26 17:29:32 -0300

    recaptulação tipo boolean

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+//boolean
+//true = verdadeiro
+//false = falso
+
+const num1 = 11;
+const num2 = 11;
+const num3 = 22;
+
+console.log((num1 + num2) === num3)
+
+const nome1 = "gabriele";
+const nome2 = "Gabriele";
+
+console.log(nome1 === nome2)
