96be33ee28f1ec4fdb3a81c012e3416813d9fddc
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-27 08:05:34 -0300

    curiosidades sobre o javascript

---
+++ SobreOjavascript.js
@@ -0,0 +1,41 @@
+//JavaScript é uma linguagem de tipagem dinâmica
+//significa que o JS identifica e considera o número de uma forma dinâmica.
+//Permitindo que o tipo e valor possam ser reatribuidos.
+// como podemos ver abaixo:
+//a variavel recebeu outro valor e outro tipo(de number para string)
+ let minhaVar = 567;
+ minhaVar = "texto";
+
+
+ //JS é uma linguagem UNTYPED
+//  SIGNIFICA QUE NÃO É NECESSARIO DECLARAR O TIPO DE SUAS VARIAVEIS, POIS ELE CONSEGUE IDENTIFICAR, POR EXEMPLO:
+// let minhavar1 number = 123;
+
+
+// é uma linguagem MULTIPARADIGMA
+//significa que é possivel resolver o mesmo problema de várias formas diferentes
+// de formas mais simples ou mais complexas
+
+
+// O JavaScript teve vários nomes. O primeiro foi Mocha, o segundo foi LiveScript, e quando a Netscape passou a ter suporte á
+// tecnologia Java em seu navegador, mudou o nome para JavaScript, como um jogo de marketing, para popularizar o script,
+// pos o Java estava em alta na época, e aproveitou o hype da época.
+
+
+///ES6 é uma versão que ficou muito famosa, devido sua importância que elevou grandemenete o patamar do JavaScript (foi lançada em 2015)
+
+
+// O JS é uma linguagem interpretada. 
+
+//linguagem interpretada: ao codigo ser escrito, ele será executado por uma programa por trás que lê as linhas de codigos e executa oq está escrito
+
+//linguagens compiladas: o código passa primeiramente por um programa chamado compilador, que transforma o código em linguagem de máquina
+// essa linguagem de máquina facilita para o computador, mas dificulta para o codificador. 
+
+// ---------------------------------------
+
+// NodeJS
+//usado para rodar o js do lado do servidor, ou seja é usado no back-end.
+// Node.js é um software de código aberto, multiplataforma, baseado no interpretador V8 do Google
+//  e que permite a execução de códigos JavaScript fora de um navegador web. 
+// A principal característica do Node.js é sua arquitetura assíncrona e orientada por eventos.
