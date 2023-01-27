281d00dd93edea1523c6b7ae8a813fa2b70b7724
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-27 07:29:34 -0300

    conversoes de tipos

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+// TIPO DE DADO
+//BOOLEANO
+
+//conversão implícita
+const numero = 456;
+const numeroString = Number("456")
+console.log(numero + numeroString)
+// se somarmos uma string com um number o js converte outros tipos em string e faz uma concatenação
+
+//Number()
+//String()
+console.log(numero + numeroString);
+
+
+//conversão explícita
