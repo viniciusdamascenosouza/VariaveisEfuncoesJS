7a76823c24b006643b409745fd8f883ae4929c86
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-27 08:24:38 -0300

    erros e stacktrace

---
+++ erros.js
@@ -0,0 +1,17 @@
+//ERROS DE SINTAXE
+
+const numero = 10;
+
+console.log(numero;
+
+
+const numero2;
+console.log(numero2)
+
+// ERRO DE REFERENCIA
+
+console.log(minhaNaoExistente)
+
+
+//STACKTRACE é todo caminho que o erro exibido no terminal nos mostra todo arquivo interno que estão sendo usados,
+// e pode mostrar em alguns momentos um bug.
