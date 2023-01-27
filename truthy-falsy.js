7837eea383c6a32baeaa87c9c7073638af0c319c
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-27 07:10:13 -0300

    truthy e falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,29 @@
+//boolean
+const usuarioLogado = true;
+
+const contaPaga = false;
+
+//truthy ou falsy
+//0=>false
+//1=>true
+
+console.log(0 == false)
+console.log("" == false)
+
+//null ==> vazio ou nada
+let outroTesteNull = null;
+console.log(outroTesteNull)
+
+
+//undefined
+let testeParaNull;
+console.log(testeParaNull);
+
+let numero = 3;
+let texto = "Alura"; 
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+console.log(typeof testeParaNull);
+console.log(typeof outroTesteNull);
