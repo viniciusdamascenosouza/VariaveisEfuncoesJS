13588f39b152b73c5d01c5cfc0cf3f3afa92f83f
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-30 07:04:46 -0300

    Operador Ternário

---
+++ ternario.js
@@ -0,0 +1,16 @@
+const idadeMinima = 18;
+const idadeCliente1 = 16;
+const idadeCliente2 = 28;
+
+if(idadeCliente1 >= idadeMinima){
+    console.log("Cliente 1 = Cerveja")
+} else{
+    console.log("Cliente 1 = Suco")
+}
+//                  condição                  TRUE                  FALSE
+console.log(idadeCliente2 >= idadeMinima ? "Cliente 2 = Cerveja" : "Cliente 2 = Suco")
+// 1°condição;
+// ? = oque acontece se for verdadeiro ou falso;
+// Primeiro oq acontece se for verdadeiro;
+// Segundo oq acontece se for falso;
+// dois pontos(:) separa um acontecimento do outro;
