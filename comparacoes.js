e3b9a181cb9cec47c98afdb970ba6294001354f0
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-30 06:35:20 -0300

    aula3 /4

---
+++ comparacoes.js
@@ -0,0 +1,19 @@
+// comparação implicita
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero === texto);
+
+// type of
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+// == Só compara o valor
+// === Compara o valor e o tipo de dado
+
+
+// Conversão explícita
+Number();
+String();
