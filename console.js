e4069272ff56759eb7ec2cf57f8ccf01d806c85a
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-30 06:21:18 -0300

    aula3 /3

---
+++ console.js
@@ -0,0 +1,11 @@
+// log => registro
+
+const minhaVar = true;
+
+ console.log(321);
+ console.log("texto");
+console.log(minhaVar)
+
+//tratamenro de erro!
+
+console.error("Deu erro!")
--- erros.js
+++ erros.js
@@ -2,10 +2,10 @@
 
 const numero = 10;
 
-console.log(numero;
+console.log(numero);
 
 
-const numero2;
+const numero2 = 10;
 console.log(numero2)
 
 // ERRO DE REFERENCIA
@@ -15,3 +15,14 @@ console.log(minhaNaoExistente)
 
 //STACKTRACE é todo caminho que o erro exibido no terminal nos mostra todo arquivo interno que estão sendo usados,
 // e pode mostrar em alguns momentos um bug.
+
+// ALGUNS TIPOS DE ERROS MAIS COMUNS:
+
+// RangeError: Quando o código recebe um dado do tipo certo, porém não dentro do formato aceitável. Por exemplo, um processamento que só pode ser feito com números inteiros maiores ou iguais a zero, mas recebe -1.
+
+// ReferenceError: Normalmente ocorre quando o código tenta acessar algo que não existe, como uma variável que não foi definida; muitas vezes é causado por erros de digitação ou confusão nos nomes utilizados, mas também pode indicar um erro no programa.
+
+// SyntaxError: Na maior parte dos casos ocorre quando há erros no programa e o JavaScript não consegue executá-lo. Os erros podem ser métodos ou propriedades escritos ou utilizados de forma incorreta, por exemplo, operadores ou sinais gráficos com elementos a menos, como esquecer de fechar chaves ou colchetes.
+
+// TypeError: Indica que o código esperava receber um dado de um determinado tipo, tal qual uma string de texto, mas recebeu outro, como um número, booleano ou null.
+
