43cd704fd1c2e59bab15a2c5708c1ed93fa43c3d
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-01-30 08:03:21 -0300

    Funções

---
+++ funcoes.js
@@ -0,0 +1,18 @@
+// let x = "";
+// console.log(x);
+// x = "OI"
+
+
+function imprimeTexto (texto) {
+    console.log(texto)
+}
+
+// a função imprime texto imorimindo o resultado da função soma:
+//Tranformei o resultado em string:
+imprimeTexto(String(soma()));
+
+// o JS é possível escrever as funções de três formas
+
+function soma () {
+    return 2 + 2;
+}
